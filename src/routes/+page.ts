61041ae812f8e4e8ef7749f5876593cdea5d95b0
HavitOngit <havitonline@gmail.com>
2023-08-21 19:21:10 +0530

    video loded

---
+++ src/routes/+page.ts
@@ -0,0 +1,20 @@
+import type { PageLoad } from './$types';
+
+export const load = (async () => {
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization:
+                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmIyODg4NjI0YTIwMjM2YWJmMGFiMGZkZDhiZWJlZiIsInN1YiI6IjY0ZTIxYjljOGMwYTQ4MDBlMzkxYmNmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v6bINn1yuhS98udYOiPoVt1blDFl3NwabfL205rnVNY'
+        }
+    };
+
+    const data = await fetch(
+        'https://api.themoviedb.org/4/discover/movie?include_adult=true&include_video=&language=en-US&page=1&with_cast&sort_by=popularity.desc',
+        options
+    )
+    const jsonData = await data.json()
+    return jsonData
+}) satisfies PageLoad;
